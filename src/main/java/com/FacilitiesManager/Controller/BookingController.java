c5a79895c115f2cbe53fdc1168dd6dbb1e466a29
Mehul Jain <Mehul@YISCINTERN25LT.Yash.com>
2024-12-02 17:55:12 +0530

    Feature update

--- src/main/java/com/FacilitiesManager/Controller/BookingController.java
+++ src/main/java/com/FacilitiesManager/Controller/BookingController.java
@@ -13,7 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/admin/booking")
+@RequestMapping("/api/admin/booking")
 public class BookingController {
 
     @Autowired
