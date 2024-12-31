2f1b7e160648cbb0bab619758dfe0c7988126a27
Mehul Jain <Mehul@YISCINTERN25LT.Yash.com>
2024-12-31 13:37:57 +0530

    Production build update

--- src/main/java/com/FacilitiesManager/FacilitiesManagerApplication.java
+++ src/main/java/com/FacilitiesManager/FacilitiesManagerApplication.java
@@ -1,13 +1,21 @@
 
+
 package com.FacilitiesManager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.FacilitiesManager")
-public class FacilitiesManagerApplication {
+public class FacilitiesManagerApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(FacilitiesManagerApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FacilitiesManagerApplication.class, args);
